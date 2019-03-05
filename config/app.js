411c37533d62c94306c79e65d5930d8b89a85c9c
Mithilesh Kohale <beingtmk@gmail.com>
2019-03-05 17:59:28 +0530

    configure app_name and container names

--- config/app.js
+++ config/app.js
@@ -1,5 +1,5 @@
 export default {
-  name: 'Apollo Starter Kit',
+  name: 'Lenshood',
   logging: {
     level: ['production'].indexOf(process.env.NODE_ENV) < 0 ? 'debug' : 'info',
     debugSQL: false,
