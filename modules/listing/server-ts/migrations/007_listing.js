7e5c4256625097272b49b0d1b2449abca173f563
Mithilesh Kohale <beingtmk@gmail.com>
2019-04-05 22:16:38 +0530

    created migration file for listing

---
+++ modules/listing/server-ts/migrations/007_listing.js
@@ -0,0 +1,101 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema
+      .createTable('listing', table => {
+        table.increments();
+        table.string('gear_category');
+        table.string('gear_subcategory');
+        table.string('description');
+        table.string('content');
+        table.timestamps(false, true);
+      })
+      .createTable('listing_image', table => {
+        table.increments();
+        table
+          .integer('listing_id')
+          .unsigned()
+          .references('id')
+          .inTable('listing')
+          .onDelete('CASCADE');
+        table.string('image_url');
+        table.timestamps(false, true);
+      })
+      .createTable('listing_detail', table => {
+        table.increments();
+        table
+          .integer('listing_id')
+          .unsigned()
+          .references('id')
+          .inTable('listing')
+          .onDelete('CASCADE');
+        table.string('condition');
+        table.string('repair_history');
+        table.string('age');
+        table.timestamps(false, true);
+      })
+      .createTable('listing_damage', table => {
+        table.increments();
+        table
+          .integer('listing_detail_id')
+          .unsigned()
+          .references('id')
+          .inTable('listing_detail')
+          .onDelete('CASCADE');
+        table.string('image_url');
+        table.string('damage_detail');
+        table.timestamps(false, true);
+      })
+      .createTable('listing_rental', table => {
+        table.increments();
+        table
+          .integer('listing_id')
+          .unsigned()
+          .references('id')
+          .inTable('listing')
+          .onDelete('CASCADE');
+        table.integer('per_day');
+        table.integer('per_week');
+        table.integer('per_month');
+        table.integer('replacement_value');
+        table.timestamps(false, true);
+      })
+      .createTable('listing_content', table => {
+        table.increments();
+        table
+          .integer('listing_id')
+          .unsigned()
+          .references('id')
+          .inTable('listing')
+          .onDelete('CASCADE');
+        table.integer('gear');
+        table.integer('brand');
+        table.integer('model');
+        table.integer('serial');
+        table.timestamps(false, true);
+      })
+      .createTable('review', table => {
+        table.increments();
+        table
+          .integer('listing_id')
+          .unsigned()
+          .references('id')
+          .inTable('listing')
+          .onDelete('CASCADE');
+        table.string('comment');
+        table.string('rating');
+        table.timestamps(false, true);
+      })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('listing'),
+    knex.schema.dropTable('listing_image'),
+    knex.schema.dropTable('listing_detail'),
+    knex.schema.dropTable('listing_damage'),
+    knex.schema.dropTable('listing_rental'),
+    knex.schema.dropTable('listing_content'),
+    knex.schema.dropTable('review')
+  ]);
+};
