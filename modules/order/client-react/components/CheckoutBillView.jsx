1bd793d9c74328915d42f8619ea939ba29634a88
Bishal Deb <bishaldeb656@gmail.com>
2019-04-13 21:48:14 +0530

    Order module added.

---
+++ modules/order/client-react/components/CheckoutBillView.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import Helmet from "react-helmet";
+import { PageLayout } from "@gqlapp/look-client-react";
+// import { TranslateFunction } from '@gqlapp/i18n-client-react';
+import settings from "../../../../settings";
+import { Row, Col, Icon, Button } from "antd";
+import CheckoutSteps from "./CheckoutSteps";
+import OrderCard from "./OrderCard";
+import naruto2 from "../resources/naruto2.jpg";
+
+const renderMetaData = () => (
+  <Helmet
+    title={`${settings.app.name} - Bill`}
+    meta={[
+      { name: "description", content: `${settings.app.name} - ${"meta"}` }
+    ]}
+  />
+);
+
+export default class CheckoutBillView extends React.Component {
+  state = {
+    product: {
+      name:
+        "Canon EOS 70D DSLR Camera Bundle with Canon EF-S 18-55mm f/3.5- 5.6 IS ",
+      image: naruto2,
+      days: 4,
+      date: {
+        start: "04 Jan'19",
+        end: "07 Jan'19"
+      },
+      refund: 5000,
+      totalRent: 1300
+    },
+    address: {
+      name: "Ankit Jain",
+      address1: "Room A308, Manas Hostel, IITG",
+      address2: "Guwahati, North Guwahati",
+      state: "Assam",
+      PIN: "7810390",
+      mobile: "+91-9085626859"
+    }
+  };
+
+  render() {
+    return (
+      <PageLayout>
+        {renderMetaData()}
+        <div className="checkoutDiv">
+          <Row gutter={32}>
+            <CheckoutSteps step={1} />
+            <Col span={24}>
+              <h3 className="billingAddress">Billing Address</h3>
+              <br />
+            </Col>
+            <Col lg={14} sm={24}>
+              <Row gutter={32}>
+                <Col
+                  xs={{ span: 18, offset: 3 }}
+                  sm={{ span: 12, offset: 0 }}
+                  md={{ span: 10, offset: 0 }}
+                  className="homeCol"
+                >
+                  <div className="HomeAddress">
+                    <div className="HomeAddressBlock">
+                      Home Address <Icon type="home" className="homeicon" />
+                    </div>
+                    <br />
+                    <div className="addressLines">
+                      <h4>{this.state.address.name},</h4>
+                      <h4>{this.state.address.address1},</h4>
+                      <h4>{this.state.address.address2},</h4>
+                      <h4>{this.state.address.state}.</h4>
+                      <h4>{this.state.address.PIN}</h4>
+                      <h4>Mobile: {this.state.address.mobile}</h4>
+                    </div>
+                    <Button className="addressIcons" ghost>
+                      <Icon type="delete" />
+                    </Button>
+                    <Button className="addressIcons" ghost>
+                      <Icon type="edit" />
+                    </Button>
+                  </div>
+                </Col>
+                <Col
+                  xs={{ span: 14, offset: 5 }}
+                  sm={{ span: 10, offset: 0 }}
+                  md={{ span: 8, offset: 0 }}
+                  className="homeCol"
+                >
+                  <div
+                    className="AddNewAddressBlock"
+                    //   onClick={}
+                  >
+                    <div className="AddNewAddress">
+                      <Icon type="plus" />
+                    </div>
+                    <h5>Add a new address</h5>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+            <Col
+              lg={{ span: 8, offset: 0 }}
+              sm={{ span: 18, offset: 3 }}
+              xs={{ span: 22, offset: 1 }}
+            >
+              <OrderCard
+                product={this.state.product}
+                paid={false}
+                buttonText={"Continue"}
+              />
+            </Col>
+          </Row>
+        </div>
+      </PageLayout>
+    );
+  }
+}
---
+++ modules/order/client-react/components/CheckoutCartView.jsx
@@ -0,0 +1,174 @@
+import React from "react";
+import Helmet from "react-helmet";
+import { PageLayout } from "@gqlapp/look-client-react";
+// import { TranslateFunction } from "@gqlapp/i18n-client-react";
+import settings from "../../../../settings";
+import { Link } from "react-router-dom";
+import { Row, Col, Button, Card, Icon } from "antd";
+import CheckoutSteps from "./CheckoutSteps";
+import naruto2 from "../resources/naruto2.jpg";
+
+const renderMetaData = () => (
+  <Helmet
+    title={`${settings.app.name} - Cart`}
+    meta={[
+      { name: "description", content: `${settings.app.name} - ${"meta"}` }
+    ]}
+  />
+);
+
+export default class CheckoutCartView extends React.Component {
+  state = {
+    products: {
+      name: "Just a random name to fill the space",
+      image: naruto2,
+      days: 4,
+      date: {
+        start: "04 Jan'19",
+        end: "07 Jan'19"
+      },
+      refund: 3000,
+      rent: 200
+    }
+  };
+
+  cartTotal() {
+    return (
+      <Card className="margin20">
+        <h2 className="cartSum">Cart Summary</h2>
+        <div className="font12">
+          <p>
+            Rent per day{" "}
+            <div className="rightfloat">&#8377; {this.state.products.rent}</div>
+          </p>
+          <p>
+            Service fee <div className="rightfloat">&#8377; 100</div>
+          </p>
+          <p>
+            &#8377; {this.state.products.rent}/- <Icon type="close" />{" "}
+            {this.state.products.days}days{" "}
+            <div className="rightfloat">
+              &#8377; {this.state.products.rent * this.state.products.days}
+            </div>
+          </p>
+          <p>
+            25% Multi day discount{" "}
+            <div className="colorFloat">
+              -&#8377;
+              {0.25 * this.state.products.rent * this.state.products.days}
+            </div>
+          </p>
+          <br />
+          <h3>
+            Total rent amount{" "}
+            <strong className="colorFloat">
+              &#8377;
+              {this.totalAmount(this.state.products)}
+            </strong>
+          </h3>
+          <p>
+            Refundable deposit{" "}
+            <div className="rightfloat">
+              &#8377; {this.state.products.refund}
+            </div>
+          </p>
+        </div>
+        <br />
+        <Button className="themeColor" block>
+          Checkout
+        </Button>
+      </Card>
+    );
+  }
+  totalAmount(cart) {
+    let total = cart.rent * cart.days;
+    total -= 0.25 * total;
+    total += 100;
+    return total;
+  }
+  render() {
+    return (
+      <PageLayout>
+        {renderMetaData()}
+        <div className="checkoutDiv">
+          <Row>
+            <CheckoutSteps step={0} />
+            <Col span={12} className="font14">
+              <div>
+                <strong>My cart - </strong>1 items
+              </div>
+              <div>
+                Total rent: <strong>&#8377; {this.state.products.rent} </strong>
+              </div>
+            </Col>
+            <Col span={12}>
+              <Link to="" className="AddMoreproducts">
+                Add more products
+              </Link>
+            </Col>
+            <Col md={13} sm={{ span: 22, offset: 1 }} className="margin20">
+              <CartItem products={this.state.products} />
+            </Col>
+            <Col
+              lg={7}
+              md={8}
+              sm={{ span: 18, offset: 3 }}
+              xs={{ span: 22, offset: 1 }}
+            >
+              {this.cartTotal()}
+            </Col>
+          </Row>
+        </div>
+      </PageLayout>
+    );
+  }
+}
+
+class CartItem extends React.Component {
+  render() {
+    return (
+      <Row className="cartitem">
+        <Col span={8}>
+          <img alt="" src={this.props.products.image} />
+        </Col>
+        <Col span={16} className="ColPad">
+          <Row className="font12">
+            <Col span={16}>
+              <h3 className="productname">{this.props.products.name}</h3>
+            </Col>
+            <Col lg={10} sm={12} xs={13} className="font11h">
+              Refundundable deposit
+              <br />
+              <strong className="colorCursor">
+                &#8377; {this.props.products.refund}
+              </strong>
+              <br />
+              <br />
+              Rental period <br />
+              <strong>
+                {this.props.products.date.start} -{" "}
+                {this.props.products.date.end}
+              </strong>
+            </Col>
+            <Col lg={10} sm={12} xs={11} className="font11h">
+              Rent <br />
+              <strong>&#8377; {this.props.products.rent}</strong>
+              <br />
+              <br />
+              Days: <br />
+              <strong>{this.props.products.days}</strong>
+            </Col>
+          </Row>
+        </Col>
+        <div className="listcloseicons">
+          <Button className="borderzero">
+            <Icon type="edit" />
+          </Button>
+          <Button className="borderzero">
+            <Icon type="close" />
+          </Button>
+        </div>
+      </Row>
+    );
+  }
+}
