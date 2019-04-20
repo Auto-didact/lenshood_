1a1bbebcabc44d47d45f0d56d133433f1367ed49
Mithilesh Kohale <beingtmk@gmail.com>
2019-04-20 15:11:23 +0530

    added account layout and my-listings static

--- modules/listing/client-react/components/MyListings.web.jsx
+++ modules/listing/client-react/components/MyListings.web.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Layout, Row, Col, Breadcrumb } from 'antd';
+import { AccountLayout } from '@gqlapp/look-client-react';
 import './resources/listingCatalogue.css';
 import MyList from './components/MyList';
 import AccDetailsMenu from './components/AccDetailsMenu';
@@ -47,22 +48,25 @@ class MyListings extends Component {
   };
   render() {
     return (
-      <div style={{ padding: '20px' }}>
-        <Breadcrumb separator=">">
-          <Breadcrumb.Item>Account</Breadcrumb.Item>
-          <Breadcrumb.Item href=""> My listing</Breadcrumb.Item>
-        </Breadcrumb>
-        <Layout style={{ padding: '24px 0', background: '#fff' }}>
-          <Row style={{ padding: '0 5%' }}>
-            <Col lg={7} md={24}>
-              <AccDetailsMenu select={3} />
-            </Col>
-            <Col lg={15} md={24}>
-              <MyList products={this.state.products} />
-            </Col>
-          </Row>
-        </Layout>
-      </div>
+      // <div style={{ padding: '20px' }}>
+      //   <Breadcrumb separator=">">
+      //     <Breadcrumb.Item>Account</Breadcrumb.Item>
+      //     <Breadcrumb.Item href=""> My listing</Breadcrumb.Item>
+      //   </Breadcrumb>
+      //   <Layout style={{ padding: '24px 0', background: '#fff' }}>
+      //     <Row style={{ padding: '0 5%' }}>
+      //       <Col lg={7} md={24}>
+      //         <AccDetailsMenu select={3} />
+      //       </Col>
+      //       <Col lg={15} md={24}>
+      //         <MyList products={this.state.products} />
+      //       </Col>
+      //     </Row>
+      //   </Layout>
+      // </div>
+      <AccountLayout select="/my-listings">
+        <MyList products={this.state.products} />
+      </AccountLayout>
     );
   }
 }
