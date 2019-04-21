daa49358232f726543f3103e5fce0212c5be9ce7
Bishal Deb <bishaldeb656@gmail.com>
2019-04-21 17:17:18 +0530

    fixed image height in carousel.

--- modules/listing/client-react/components/components/ProductCard.js
+++ modules/listing/client-react/components/components/ProductCard.js
@@ -15,10 +15,11 @@ class ProductCard extends Component {
         <Carousel>
           {images.map(item => (
             <div>
-              <img src={item.imageUrl} alt="" className="width100" />
+              <img src={item.imageUrl} alt="" className="CaraImg" />
             </div>
           ))}
         </Carousel>
+        <br />
         <Row>
           <Col span={12}>
             <strong className="mainColor font12">Replacement Value</strong>
@@ -47,9 +48,7 @@ class ProductCard extends Component {
         <h3 className="font16 blockDisplay fontBold">In the package</h3>
         {packageContents.map(item => (
           <div className="Inline-blockDisplay width50 font14">
-            <span
-             className="itemGearPC"
-            />
+            <span className="itemGearPC" />
             {item.gear}
           </div>
         ))}
