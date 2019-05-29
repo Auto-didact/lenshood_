c628ac47dbf79768b369d67bc98e4fcf35ee2b82
lokeshrana9999 <lokeshrana9999@gmail.com>
2019-05-29 11:33:40 +0530

    UserEditForm completed right up till uploading data

--- modules/user/client-react/components/ProfileHead.web.jsx
+++ modules/user/client-react/components/ProfileHead.web.jsx
@@ -1,45 +1,36 @@
-import React from "react";
-import { CardGroup } from "@gqlapp/look-client-react";
-import { Avatar, Icon, Row, Col, Divider } from "antd";
+import React from 'react';
+import { CardGroup } from '@gqlapp/look-client-react';
+import { Avatar, Icon, Row, Col, Divider } from 'antd';
 
 const ProfileHead = ({ profile, description }) => {
   return (
-    <div align="center" style={{ marginBottom: "10px" }}>
+    <div align="center" style={{ marginBottom: '10px' }}>
       <Avatar
         size={100}
         src={
-          profile && profile.image
-            ? profile.image
-            : "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+          profile && profile.image ? profile.image : 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
         }
       />
       <br />
 
       <CardGroup>
-        <h2 style={{ textAlign: "center" }}>
+        <h2 style={{ textAlign: 'center' }}>
           {profile && profile.firstName && profile.lastName
-            ? profile.firstName + " " + profile.lastName
-            : "Not Provided"}
+            ? profile.firstName + ' ' + profile.lastName
+            : 'Not Provided'}
         </h2>
       </CardGroup>
 
-      <h4 style={{ textAlign: "center" }}>
-        ({profile && profile.designation ? profile.designation : "Not Provided"}
-        )
-      </h4>
+      <h4 style={{ textAlign: 'center' }}>({profile && profile.designation ? profile.designation : 'Not Provided'})</h4>
       <Divider />
       <Row>
         <Col
           span={8}
           style={{
-            align: "center"
+            align: 'center'
           }}
         >
-          <h2>
-            {profile && profile.acceptanceRate
-              ? profile.acceptanceRate
-              : "Not Available"}
-          </h2>
+          <h2>{profile && profile.acceptanceRate ? profile.acceptanceRate : 'Not Available'}</h2>
 
           <h4>{description.acceptanceRate}</h4>
         </Col>
@@ -47,13 +38,13 @@ const ProfileHead = ({ profile, description }) => {
         <Col
           span={8}
           style={{
-            align: "center"
+            align: 'center'
           }}
         >
           <div>
             <h2>
               <span className="StarRate">
-                {profile && profile.rating ? profile.rating : "Not Rated"}
+                {profile && profile.rating ? profile.rating : 'Not Rated'}
                 <Icon type="star" theme="filled" />
               </span>
             </h2>
@@ -65,12 +56,10 @@ const ProfileHead = ({ profile, description }) => {
         <Col
           span={8}
           style={{
-            align: "center"
+            align: 'center'
           }}
         >
-          <h2>
-            {profile && profile.rating ? profile.rating : "Not Available "}
-          </h2>
+          <h2>{profile && profile.rating ? profile.rating : 'Not Available '}</h2>
           <h4>{description.responseTime}</h4>
         </Col>
       </Row>
