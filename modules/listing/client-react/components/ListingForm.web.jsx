cbb093986ca4d32f40150029d3bc5bc4f9da69e3
lokeshrana9999 <lokeshrana9999@gmail.com>
2019-05-30 03:18:24 +0530

    role error resolved

--- modules/listing/client-react/components/ListingForm.web.jsx
+++ modules/listing/client-react/components/ListingForm.web.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withFormik } from 'formik';
-import { translate } from '@gqlapp/i18n-client-react';
-import { required, validate } from '@gqlapp/validation-common-react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withFormik } from "formik";
+import { translate } from "@gqlapp/i18n-client-react";
+import { required, validate } from "@gqlapp/validation-common-react";
 
-import { Form, Button } from '@gqlapp/look-client-react';
+import { Form, Button } from "@gqlapp/look-client-react";
 // Abstract Out
-import { Row, Col, Icon, message } from 'antd';
+import { Row, Col, Icon, message } from "antd";
 
-import ProductDetails from './components/ListingForm/ProductDetails';
-import RentalDetails from './components/ListingForm/RentalDetails';
-import ListYGSteps from './components/ListYGSteps';
+import ProductDetails from "./components/ListingForm/ProductDetails";
+import RentalDetails from "./components/ListingForm/RentalDetails";
+import ListYGSteps from "./components/ListYGSteps";
 
 const ProductDetailsSchema = {
   gearCategory: [required],
@@ -32,7 +32,7 @@ class ListingForm extends Component {
     // console.log(errors);
     // console.log(this.props.values);
     if (isErrorsEmpty) this.setState(state => ({ step: state.step + 1 }));
-    else message.info('Fill in the Required details before moving on!');
+    else message.info("Fill in the Required details before moving on!");
 
     // set errors and touched
   };
@@ -41,7 +41,7 @@ class ListingForm extends Component {
   };
 
   isAdminFunction = role => {
-    if (role === 'admin') {
+    if (role === "admin") {
       return true;
     } else {
       return false;
@@ -50,7 +50,9 @@ class ListingForm extends Component {
 
   render() {
     const { values, handleSubmit, submitting, t, currentUser } = this.props;
-    const isAdmin = this.isAdminFunction(currentUser.role);
+    const isAdmin = this.isAdminFunction(
+      currentUser && currentUser.role ? currentUser.role : null
+    );
     // console.log("currentUser", this.props);
     // const userRole = currentUser.role;
     this.steps = [
@@ -61,7 +63,11 @@ class ListingForm extends Component {
     return (
       <div className="Listyourgearcards">
         <Row>
-          <Col md={{ span: 14, offset: 5 }} sm={{ span: 20, offset: 2 }} className="LYGcol1">
+          <Col
+            md={{ span: 14, offset: 5 }}
+            sm={{ span: 20, offset: 2 }}
+            className="LYGcol1"
+          >
             <ListYGSteps step={this.state.step + 1} />
 
             <Form name="listing" layout="vertical" onSubmit={handleSubmit}>
@@ -71,18 +77,27 @@ class ListingForm extends Component {
                 <>
                   <Button color="secondary" onClick={this.prevStep}>
                     <Icon type="left-circle" />
-                    {t('listing.btn.prev')}
+                    {t("listing.btn.prev")}
                   </Button>
 
                   {/* abstract out styles To Do, and arrows to button */}
-                  <Button color="primary" type="submit" disabled={submitting} style={{ float: 'right' }}>
-                    {t('listing.btn.submit')}
+                  <Button
+                    color="primary"
+                    type="submit"
+                    disabled={submitting}
+                    style={{ float: "right" }}
+                  >
+                    {t("listing.btn.submit")}
                     <Icon type="enter" />
                   </Button>
                 </>
               ) : (
-                <Button color="primary" onClick={this.nextStep} style={{ float: 'right' }}>
-                  {t('listing.btn.next')}
+                <Button
+                  color="primary"
+                  onClick={this.nextStep}
+                  style={{ float: "right" }}
+                >
+                  {t("listing.btn.next")}
                   <Icon type="right-circle" />
                 </Button>
               )}
@@ -110,7 +125,7 @@ const ListingFormWithFormik = withFormik({
     gearCategory: props.listing && props.listing.gearCategory,
     gearSubcategory: props.listing && props.listing.gearSubcategory,
     description: props.listing && props.listing.description,
-    status: (props.listing && props.listing.status) || 'Idle',
+    status: (props.listing && props.listing.status) || "Idle",
     isActive: (props.listing && props.listing.isActive) || true,
     listingImages: props.listing && props.listing.listingImages,
     listingDetail: (props.listing && props.listing.listingDetail) || {},
@@ -128,7 +143,7 @@ const ListingFormWithFormik = withFormik({
     onSubmit(values);
   },
   enableReinitialize: true,
-  displayName: 'ListingForm' // helps with React DevTools
+  displayName: "ListingForm" // helps with React DevTools
 });
 
-export default translate('listing')(ListingFormWithFormik(ListingForm));
+export default translate("listing")(ListingFormWithFormik(ListingForm));
