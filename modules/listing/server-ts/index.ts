f928c1b21f88657dac64bce2096e52e0f631afb1
Mithilesh Kohale <beingtmk@gmail.com>
2019-05-30 00:26:11 +0530

    working on adding userID to listing

--- modules/listing/server-ts/index.ts
+++ modules/listing/server-ts/index.ts
@@ -2,10 +2,19 @@ import ServerModule from '@gqlapp/module-server-ts';
 
 import schema from './schema.graphql';
 import createResolvers from './resolvers';
+import scopes from './scopes';
 import Listing from './sql';
+
+const createContextFunc = ({ graphqlContext: { identity } }) => ({
+  Listing: new Listing(),
+  auth: {
+    isAuthenticated: !!identity,
+    scope: identity && identity.role ? scopes[identity.role] : null
+  }
+});
 
 export default new ServerModule({
   schema: [schema],
   createResolversFunc: [createResolvers],
-  createContextFunc: [() => ({ Listing: new Listing() })]
+  createContextFunc: [createContextFunc]
 });
