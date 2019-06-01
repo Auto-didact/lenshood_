9c4ca888b5f2df3a4b5873cad4d07b1526d43f67
Bishal Deb <bishaldeb656@gmail.com>
2019-06-01 17:35:33 +0530

    Date range changes done.

--- modules/order/client-react/components/CheckoutCartView.jsx
+++ modules/order/client-react/components/CheckoutCartView.jsx
@@ -4,12 +4,11 @@ import { PageLayout } from "@gqlapp/look-client-react";
 // import { TranslateFunction } from "@gqlapp/i18n-client-react";
 import settings from "../../../../settings";
 import { Link } from "react-router-dom";
-import { Row, Col, Button, Card, Icon, DatePicker } from "antd";
+import { Row, Col, Button, Card, Icon } from "antd";
 import CheckoutSteps from "./CheckoutSteps";
+import CartItem from "./CartItem";
 import camera from "../resources/camera.jpg";
 import { TotalAmount, TotalRent, Refund } from "../helper/index";
-
-const { RangePicker } = DatePicker;
 
 const renderMetaData = () => (
   <Helmet
@@ -23,33 +22,38 @@ const renderMetaData = () => (
 export default class CheckoutCartView extends React.Component {
   state = {
     cart: {
-      name: "Just a random name to fill the space",
+      name: "Nikon Camera",
       image: camera,
       days: 4,
       date: {
-        start: "04 Jan'19",
-        end: "07 Jan'19"
+        start: "04-01-19",
+        end: "07-01-19"
       },
       refund: 3000,
       rent: 200
     },
     products: [
       {
-        name: "Just a random name to fill the space",
+        name: "Nikon Camera",
         image: camera,
         id: 1,
         days: 4,
         date: {
-          start: "04 Jan'19",
-          end: "07 Jan'19"
+          start: "04-01-19",
+          end: "07-01-19"
         },
         refund: 3000,
         rent: 200
       }
     ],
-    servicefee: 100
+    servicefee: 100,
+    modal1Visible: false
   };
   count = 1;
+
+  setModal1Visible() {
+    this.setState({ modal1Visible: !this.state.modal1Visible });
+  }
 
   cartTotal() {
     return (
@@ -112,8 +116,10 @@ export default class CheckoutCartView extends React.Component {
         <CartItem
           key={cartItem.id}
           products={cartItem}
+          modal1Visible={this.state.modal1Visible}
           deleteProduct={this.deleteProduct.bind(this, cartItem.id)}
-          editProduct={this.editProduct.bind(this, cartItem.id)}
+          editProduct={this.editProduct.bind(this)}
+          setModal1Visible={this.setModal1Visible.bind(this)}
         />
       );
     });
@@ -154,21 +160,19 @@ export default class CheckoutCartView extends React.Component {
     });
   }
 
-  editProduct = id => {
-    // CODE FOR EDITTING THE POST WILL GO HERE
-    console.log("Edit Product Details!!");
+  editProduct(id, startDate, EndDate) {
+    // Edit datat goes here
     var i, index;
     for (i = 0; i < this.state.products.length; i++) {
       if (this.state.products[i].id == id) {
-        index = i;
+        this.state.products[i].date.start = startDate.format("DD-MM-YY");
+        this.state.products[i].date.end = EndDate.format("DD-MM-YY");
+        this.state.products[i].days = EndDate.diff(startDate, "days") + 1;
         break;
       }
     }
-    // this.setState({
-    //   products[index].date.start:
-    //   ]
-    // });
-  };
+    this.setModal1Visible();
+  }
 
   render() {
     return (
@@ -186,9 +190,7 @@ export default class CheckoutCartView extends React.Component {
                 </div>
                 <div>
                   Total rent:{" "}
-                  <strong>
-                    &#8377; {TotalRent( this.state.products)}{" "}
-                  </strong>
+                  <strong>&#8377; {TotalRent(this.state.products)} </strong>
                 </div>
               </Col>
               <Col span={12}>
@@ -205,7 +207,6 @@ export default class CheckoutCartView extends React.Component {
             <Col
               xl={11}
               lg={{ span: 13, offset: 1 }}
-              sm={{ span: 20, offset: 1 }}
               xs={{ span: 22, offset: 1 }}
               className="margin20"
             >
@@ -213,8 +214,7 @@ export default class CheckoutCartView extends React.Component {
             </Col>
             <Col
               lg={7}
-              md={{ span: 20, offset: 0 }}
-              sm={{ span: 20, offset: 0 }}
+              sm={{ span: 22, offset: 0 }}
               xs={{ span: 22, offset: 1 }}
             >
               {this.cartTotal()}
@@ -222,61 +222,6 @@ export default class CheckoutCartView extends React.Component {
           </Row>
         </div>
       </PageLayout>
-    );
-  }
-}
-
-class CartItem extends React.Component {
-  render() {
-    return (
-      <Row className="cartitem borderRadius9">
-        <div className="listcloseicons">
-          <Button
-            onClick={() => this.props.editProduct()}
-            className="borderzero listclose"
-          >
-            <Icon type="edit" />
-          </Button>
-          <Button
-            className="borderzero listclose"
-            onClick={() => this.props.deleteProduct()}
-          >
-            <Icon type="close" />
-          </Button>
-        </div>
-        <Col sm={8} xs={24}>
-          <img alt="" src={this.props.products.image} />
-        </Col>
-        <Col sm={16} xs={24} className="ColPad">
-          <Row className="font12">
-            <Col md={16} sm={24}>
-              <h3 className="productname">{this.props.products.name}</h3>
-            </Col>
-            <Col lg={12} sm={12} xs={14} className="font11h">
-              Refundundable deposit
-              <br />
-              <strong className="colorCursor">
-                &#8377; {this.props.products.refund}
-              </strong>
-              <br />
-              <br />
-              Rental period <br />
-              <strong>
-                {this.props.products.date.start} -{" "}
-                {this.props.products.date.end}
-              </strong>
-            </Col>
-            <Col lg={10} sm={12} xs={10} className="font11h">
-              Rent <br />
-              <strong>&#8377; {this.props.products.rent}</strong>
-              <br />
-              <br />
-              Days: <br />
-              <strong>{this.props.products.days}</strong>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
     );
   }
 }
