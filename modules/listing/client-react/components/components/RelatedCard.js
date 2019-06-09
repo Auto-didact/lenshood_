052d59fd29d62a852fe265002d944edad5232464
lokeshrana9999 <lokeshrana9999@gmail.com>
2019-06-09 10:45:09 +0530

    Image size issue in Catalog page resolved and Image issue in Profile page resolved

--- modules/listing/client-react/components/components/RelatedCard.js
+++ modules/listing/client-react/components/components/RelatedCard.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-import { Card, Rate, Icon, Carousel, Avatar } from 'antd';
+import { Card, Rate, Icon, Carousel, Avatar, Divider } from "antd";
+import { ImgCamera } from "../../constants/DefaultImages";
+
 // import '../resources/listingCatalogue.css';
 
 const { Meta } = Card;
@@ -21,10 +23,10 @@ class RelatedCard extends Component {
     const listing_img =
       listing.listingImages.length !== 0 && listing.listingImages
         ? listing.listingImages[0].imageUrl
-        : 'https://cdn.pixabay.com/photo/2013/11/28/10/02/photo-camera-219958_960_720.jpg';
+        : ImgCamera[0].imageUrl;
 
-    const rent_per_day = listing.listingRental.perDay || 'Not Provided';
-    const title = listing.gearCategory || 'Not Provided';
+    const rent_per_day = listing.listingRental.perDay || "Not Provided";
+    const title = listing.gearCategory || "Not Provided";
 
     //To Do parse sellerName
     // const seller =
@@ -32,8 +34,14 @@ class RelatedCard extends Component {
     //     listing.user.profile &&
     //     listing.user.profile.firstName.concat(' ', listing.user.profile.lastName)) ||
     //   'Name Not Provided';
-    const sellerFirstName = (listing.user && listing.user.profile && listing.user.profile.firstName) || null;
-    const sellerLastName = (listing.user && listing.user.profile && listing.user.profile.lastName) || null;
+    const sellerFirstName =
+      (listing.user &&
+        listing.user.profile &&
+        listing.user.profile.firstName) ||
+      null;
+    const sellerLastName =
+      (listing.user && listing.user.profile && listing.user.profile.lastName) ||
+      null;
     const sellerName = (f, l) => {
       if (f && l) {
         return `${f} ${l}`;
@@ -44,7 +52,7 @@ class RelatedCard extends Component {
           return l;
         }
       } else {
-        return 'Name Not Provided';
+        return "Name Not Provided";
       }
     };
 
@@ -52,8 +60,10 @@ class RelatedCard extends Component {
     // const seller = "Not Provided";
     const sellerAvatar =
       (listing.user && listing.user.profile && listing.user.profile.avatar) ||
-      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png';
-    const rating = (listing.user && listing.user.profile && listing.user.profile.rating) || null;
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
+    const rating =
+      (listing.user && listing.user.profile && listing.user.profile.rating) ||
+      null;
 
     const status = {
       dots: true,
@@ -66,8 +76,34 @@ class RelatedCard extends Component {
 
     return (
       <Link className="listing-link" to={`/listing-detail/${listing_id}`}>
-        <Card bodyStyle={{ margin: '0px' }} hoverable cover={<img src={listing_img} style={{ width: '100%' }} />}>
-          <Meta title={title.substring(0, 25)} description={<h4>&#8377;{rent_per_day} per day</h4>} />
+        <Card
+          bodyStyle={{ margin: "0px" }}
+          hoverable
+          cover={
+            <div
+              style={{
+                overflow: "hidden",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "230px",
+                borderRadius: "8px 8px 0px 0px"
+              }}
+              align="center"
+            >
+              <img
+                src={listing_img}
+                style={{
+                  height: "100%"
+                }}
+              />
+            </div>
+          }
+        >
+          <Meta
+            title={title.substring(0, 25)}
+            description={<h4>&#8377;{rent_per_day} per day</h4>}
+          />
+
           <Meta
             className="RelCardMeta"
             avatar={<Avatar src={sellerAvatar} />}
@@ -75,7 +111,11 @@ class RelatedCard extends Component {
               <h3 className="CatalogUserName">
                 {seller}
                 <br />
-                {rating ? <Rate disabled defaultValue={rating} className="CardRate" /> : <p>Not Reviewed</p>}
+                {rating ? (
+                  <Rate disabled defaultValue={rating} className="CardRate" />
+                ) : (
+                  <p>Not Reviewed</p>
+                )}
                 {/* <div className="RelIconGroup">
                   <Icon type="car" theme="filled" className="marginR10" />
                   <Icon type="safety-certificate" theme="filled" />
