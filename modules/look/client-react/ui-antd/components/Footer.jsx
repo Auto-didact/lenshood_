595c2edd0e6c18815c1ffc1780cc23a48c3d710b
Fahad Israr <fahad00cms@gmail.com>
2019-06-10 17:05:20 +0530

    Footer For Mobile View

--- modules/look/client-react/ui-antd/components/Footer.jsx
+++ modules/look/client-react/ui-antd/components/Footer.jsx
@@ -1,21 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import {Menu, Dropdown, Icon } from "antd";
-import { Link } from "react-router-dom";
-import Container from "./Container";
-import Row from "./Row";
-import Col from "./Col";
-import "./styles.css";
+import { Menu, Dropdown, Icon } from 'antd';
+import { Link } from 'react-router-dom';
+import Container from './Container';
+import Row from './Row';
+import Col from './Col';
+import './styles.css';
 
 export default class Footer extends React.Component {
-
-  state={
+  state = {
     width: 0,
     height: 0,
-    show1:false,
-    show2:false,
-    show3:false,
-    show4:false,
+    show1: false,
+    show2: false,
+    show3: false,
+    show4: false
   };
 
   componentDidMount() {
@@ -34,19 +33,27 @@ export default class Footer extends React.Component {
   updateWindowDimensions = () => {
     this.setState({ width: window.innerWidth, height: window.innerHeight });
   };
-  toggle=function(det){
-if(det==1)
-  this.setState((state)=>{return{show1:!state.show1}});
-  if(det==2)
-  this.setState((state)=>{return{show2:!state.show2}});
-  if(det==3)
-  this.setState((state)=>{return{show3:!state.show3}});
-  if(det==4)
-  this.setState((state)=>{return{show4:!state.show4}});
-  }
+  toggle = function(det) {
+    if (det == 1)
+      this.setState(state => {
+        return { show1: !state.show1 };
+      });
+    if (det == 2)
+      this.setState(state => {
+        return { show2: !state.show2 };
+      });
+    if (det == 3)
+      this.setState(state => {
+        return { show3: !state.show3 };
+      });
+    if (det == 4)
+      this.setState(state => {
+        return { show4: !state.show4 };
+      });
+  };
 
   render() {
-    return this.state.width>800?(
+    return this.state.width > 800 ? (
       <Container className="no-print" align="center">
         <Row className="footer" gutter={16}>
           <Col lg={12} sm={24}>
@@ -115,26 +122,14 @@ if(det==1)
                     <Link to="/blog">Blog</Link>
                   </li>
                   <li>
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://facebook.com/lenshoodcommunity"
-                    >
-                      {" "}
+                    <a target="_blank" rel="noopener noreferrer" href="https://facebook.com/lenshoodcommunity">
+                      {' '}
                       <Icon theme="filled" type="facebook" />
                     </a>
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://www.linkedin.com/company/wearelenshood"
-                    >
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/wearelenshood">
                       <Icon theme="filled" type="linkedin" />
                     </a>
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://www.instagram.com/we_are_lenshood/"
-                    >
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/we_are_lenshood/">
                       <Icon theme="filled" type="instagram" />
                     </a>
                     <a target="_blank" rel="noopener noreferrer" href="/">
@@ -145,22 +140,18 @@ if(det==1)
               </Col>
             </Row>
           </Col>
-          <Col
-            className="FooterLogo"
-            span={24}
-            style={{ borderTopStyle: "solid", padding: "10px 0px 0px 0px" }}
-          >
+          <Col className="FooterLogo" span={24} style={{ borderTopStyle: 'solid', padding: '10px 0px 0px 0px' }}>
             <Row>
               <Link to="/">
                 <img
-                  src={require("../../logo/OrgLogo.png")}
+                  src={require('../../logo/OrgLogo.png')}
                   height="50"
                   width="50"
                   align="centre"
                   style={{
-                    display: "block",
-                    marginLeft: "auto",
-                    marginRight: "auto"
+                    display: 'block',
+                    marginLeft: 'auto',
+                    marginRight: 'auto'
                   }}
                 />
               </Link>
@@ -168,34 +159,40 @@ if(det==1)
           </Col>
         </Row>
       </Container>
-    ):(
-<Container className="no-print" align="center">
+    ) : (
+      <Container className="no-print" align="center">
         <Row className="footer" gutter={16}>
-        <div style={{display:'flex',flexDirection:'column'}}>
-          <div>
-                <h3  onClick={()=>this.toggle(1)}>About Lenshood<img src={require('../images/dropdown.png')} width="20px" height="20px" /></h3>
-                <div style={this.state.show1?{display:'block'}:{display:'none'}}>
-                    <ul style={{listStyle:'none',padding:'0 0 0 0'}} >
-                    <li>
-                      <Link to="/about-us">About us</Link>
-                    </li>
-                    <li>
-                      <Link to="/terms-of-service">Terms of Service</Link>
-                    </li>
-                    <li>
-                      <Link to="/privacy-rules">Privacy Rules</Link>
-                    </li>
-                    <li>
-                      <Link to="/mission">Mission</Link>
-                    </li>
-                    </ul>
-                </div>    
-          </div>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div>
+              <h3 onClick={() => this.toggle(1)}>
+                About Lenshood
+                <img src={require('../images/dropdown.png')} width="20px" height="20px" />
+              </h3>
+              <div style={this.state.show1 ? { display: 'block' } : { display: 'none' }}>
+                <ul style={{ listStyle: 'none', padding: '0 0 0 0' }}>
+                  <li>
+                    <Link to="/about-us">About us</Link>
+                  </li>
+                  <li>
+                    <Link to="/terms-of-service">Terms of Service</Link>
+                  </li>
+                  <li>
+                    <Link to="/privacy-rules">Privacy Rules</Link>
+                  </li>
+                  <li>
+                    <Link to="/mission">Mission</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
 
-          <div>
-          <h3  onClick={()=>this.toggle(2)}>How It Works<img src={require('../images/dropdown.png')} width="20px" height="20px" /></h3>
-                  <div style={this.state.show2?{display:'block'}:{display:'none'}}>
-                  <ul style={{listStyle:'none',padding:'0 0 0 0'}} >
+            <div>
+              <h3 onClick={() => this.toggle(2)}>
+                How It Works
+                <img src={require('../images/dropdown.png')} width="20px" height="20px" />
+              </h3>
+              <div style={this.state.show2 ? { display: 'block' } : { display: 'none' }}>
+                <ul style={{ listStyle: 'none', padding: '0 0 0 0' }}>
                   <li>
                     <Link to="/renting">Renting</Link>
                   </li>
@@ -208,13 +205,16 @@ if(det==1)
                   <li>
                     <Link to="/faq">FAQ</Link>
                   </li>
-                  </ul>
-                  </div>    
-          </div>
-          <div>
-          <h3  onClick={()=>this.toggle(3)}>Keep In Touch<img src={require('../images/dropdown.png')} width="20px" height="20px" /></h3>
-                  <div style={this.state.show3?{display:'block'}:{display:'none'}}>
-                  <ul style={{listStyle:'none',padding:'0 0 0 0'}} >
+                </ul>
+              </div>
+            </div>
+            <div>
+              <h3 onClick={() => this.toggle(3)}>
+                Keep In Touch
+                <img src={require('../images/dropdown.png')} width="20px" height="20px" />
+              </h3>
+              <div style={this.state.show3 ? { display: 'block' } : { display: 'none' }}>
+                <ul style={{ listStyle: 'none', padding: '0 0 0 0' }}>
                   <li>
                     <a href="mailto:reachus@lenshood.in">
                       Write to us:
@@ -229,68 +229,52 @@ if(det==1)
                       +91-9085626859
                     </a>
                   </li>
-                  </ul>
-                  </div>    
-          </div>
+                </ul>
+              </div>
+            </div>
 
-          <div>
-          <h3 onClick={()=>this.toggle(4)}>Follow Us<img src={require('../images/dropdown.png')} width="20px" height="20px" /></h3>
-                  <div style={this.state.show4?{display:'block'}:{display:'none'}}>
-                  <ul style={{listStyle:'none',padding:'0 0 0 0'}} >
+            <div>
+              <h3 onClick={() => this.toggle(4)}>
+                Follow Us
+                <img src={require('../images/dropdown.png')} width="20px" height="20px" />
+              </h3>
+              <div style={this.state.show4 ? { display: 'block' } : { display: 'none' }}>
+                <ul style={{ listStyle: 'none', padding: '0 0 0 0' }}>
                   <li>
                     <Link to="/blog">Blog</Link>
                   </li>
                   <li>
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://facebook.com/lenshoodcommunity"
-                    >
-                      {" "}
+                    <a target="_blank" rel="noopener noreferrer" href="https://facebook.com/lenshoodcommunity">
+                      {' '}
                       <Icon theme="filled" type="facebook" />
                     </a>
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://www.linkedin.com/company/wearelenshood"
-                    >
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/wearelenshood">
                       <Icon theme="filled" type="linkedin" />
                     </a>
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://www.instagram.com/we_are_lenshood/"
-                    >
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/we_are_lenshood/">
                       <Icon theme="filled" type="instagram" />
                     </a>
                     <a target="_blank" rel="noopener noreferrer" href="/">
                       <Icon type="twitter" />
                     </a>
                   </li>
-                  </ul>
-                  </div>    
+                </ul>
+              </div>
+            </div>
           </div>
 
-          
-          
-        </div>
-       
-          <Col
-            className="FooterLogo"
-            span={24}
-            style={{ borderTopStyle: "solid", padding: "10px 0px 0px 0px" }}
-          >
+          <Col className="FooterLogo" span={24} style={{ borderTopStyle: 'solid', padding: '10px 0px 0px 0px' }}>
             <Row>
               <Link to="/">
                 <img
-                  src={require("../../logo/OrgLogo.png")}
+                  src={require('../../logo/OrgLogo.png')}
                   height="50"
                   width="50"
                   align="centre"
                   style={{
-                    display: "block",
-                    marginLeft: "auto",
-                    marginRight: "auto"
+                    display: 'block',
+                    marginLeft: 'auto',
+                    marginRight: 'auto'
                   }}
                 />
               </Link>
