fc3e938dbc2a9c4b24d8f9f4778910d54d971a5c
lokeshrana9999 <lokeshrana9999@gmail.com>
2019-06-16 05:57:31 +0530

    ListingDetailView Helmet fixed, cancellation and damage policy added, breadcrumb data changed

--- modules/listing/client-react/components/components/AddToCartCard.js
+++ modules/listing/client-react/components/components/AddToCartCard.js
@@ -48,7 +48,7 @@ export default class AddToCartCard extends Component {
     function datediff(first, second) {
       // Take the difference between the dates and divide by milliseconds per day.
       // Round to nearest whole number to deal with DST.
-      console.log(Math.round((second - first) / (1000 * 60 * 60 * 24)));
+
       return Math.round((second - first) / (1000 * 60 * 60 * 24));
     }
     const dayDifference =
