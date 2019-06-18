c7768ec71e68d442321290e4842ade0de60395eb
Fahad Israr <fahad00cms@gmail.com>
2019-06-18 11:50:38 +0530

    User Profile Edit

--- modules/user/client-react/components/UserEditView.web.jsx
+++ modules/user/client-react/components/UserEditView.web.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import {Icon} from 'antd';
+import { Icon } from 'antd';
 import { Link } from 'react-router-dom';
 import { translate } from '@gqlapp/i18n-client-react';
 import { PageLayout } from '@gqlapp/look-client-react';
@@ -31,10 +31,14 @@ const UserEditView = ({ loading, user, t, currentUser, onSubmit }) => {
         <div className="text-center">{t('userEdit.loadMsg')}</div>
       ) : (
         <div style={{}}>
-          <Link id="back-button" to={currentUser && currentUser.role === 'admin' ? '/users' : '/profile'} style={{fontSize:'20px',margin:'10px',textDecoration:'underline'}}>
-          <Icon type="arrow-left" /> Back
+          <Link
+            id="back-button"
+            to={currentUser && currentUser.role === 'admin' ? '/users' : '/profile'}
+            style={{ fontSize: '20px', margin: '10px', textDecoration: 'underline' }}
+          >
+            <Icon type="arrow-left" /> Back
           </Link>
-          <h2 style={{textAlign:'center'}}>
+          <h2 style={{ textAlign: 'center' }}>
             {t('userEdit.form.titleEdit')} {t('userEdit.form.title')}
           </h2>
           <UserForm
--- modules/user/client-react/components/UserForm.web.jsx
+++ modules/user/client-react/components/UserForm.web.jsx
@@ -1,16 +1,10 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withFormik } from "formik";
-import { isEmpty } from "lodash";
-import { isFormError, FieldAdapter as Field } from "@gqlapp/forms-client-react";
-import { translate } from "@gqlapp/i18n-client-react";
-import {
-  email,
-  minLength,
-  required,
-  match,
-  validate
-} from "@gqlapp/validation-common-react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withFormik } from 'formik';
+import { isEmpty } from 'lodash';
+import { isFormError, FieldAdapter as Field } from '@gqlapp/forms-client-react';
+import { translate } from '@gqlapp/i18n-client-react';
+import { email, minLength, required, match, validate } from '@gqlapp/validation-common-react';
 import {
   Form,
   RenderField,
@@ -21,10 +15,10 @@ import {
   Option,
   Button,
   Alert
-} from "@gqlapp/look-client-react";
-import { FieldArray } from "formik";
-import settings from "../../../../settings";
-import './styling.css'
+} from '@gqlapp/look-client-react';
+import { FieldArray } from 'formik';
+import settings from '../../../../settings';
+import './styling.css';
 
 const userFormSchema = {
   username: [required, minLength(3)],
@@ -34,15 +28,11 @@ const userFormSchema = {
 const createUserFormSchema = {
   ...userFormSchema,
   password: [required, minLength(settings.auth.password.minLength)],
-  passwordConfirmation: [
-    required,
-    match("password"),
-    minLength(settings.auth.password.minLength)
-  ]
+  passwordConfirmation: [required, match('password'), minLength(settings.auth.password.minLength)]
 };
 
 const isAdminFunction = role => {
-  if (role === "admin") {
+  if (role === 'admin') {
     return true;
   } else {
     return false;
@@ -52,21 +42,10 @@ const isAdminFunction = role => {
 const updateUserFormSchema = {
   ...userFormSchema,
   password: minLength(settings.auth.password.minLength),
-  passwordConfirmation: [
-    match("password"),
-    minLength(settings.auth.password.minLength)
-  ]
+  passwordConfirmation: [match('password'), minLength(settings.auth.password.minLength)]
 };
 
-const UserForm = ({
-  values,
-  handleSubmit,
-  errors,
-  setFieldValue,
-  t,
-  shouldDisplayRole,
-  shouldDisplayActive
-}) => {
+const UserForm = ({ values, handleSubmit, errors, setFieldValue, t, shouldDisplayRole, shouldDisplayActive }) => {
   const {
     userRole,
     username,
@@ -85,215 +64,206 @@ const UserForm = ({
 
   return (
     <div className="con">
-    <Form name="user" onSubmit={handleSubmit}>
-      <Field
-        name="profile.avatar"
-        component={RenderUpload}
-        type="text"
-        label={t("userEdit.form.field.avatar")}
-        value={profile.avatar}
-      />
-      <Field
-        name="username"
-        component={RenderField}
-        type="text"
-        label={t("userEdit.form.field.name")}
-        value={username}
-      />
-      <Field
-        name="email"
-        component={RenderField}
-        type="email"
-        label={t("userEdit.form.field.email")}
-        value={email}
-      />
-      {isAdmin && (
+      <Form name="user" onSubmit={handleSubmit}>
         <Field
-          name="role"
-          component={RenderSelect}
-          label={t("userEdit.form.field.role.label")}
-          value={role}
-        >
-          <Option value="user">{t("userEdit.form.field.role.user")}</Option>
-          <Option value="admin">{t("userEdit.form.field.role.admin")}</Option>
-        </Field>
-      )}
-
-      <Field
-        name="profile.firstName"
-        component={RenderField}
-        type="text"
-        label={t("userEdit.form.field.firstName")}
-        value={profile.firstName}
-      />
-      <Field
-        name="profile.lastName"
-        component={RenderField}
-        type="text"
-        label={t("userEdit.form.field.lastName")}
-        value={profile.lastName}
-      />
-
-      <Field
-        name="profile.about"
-        component={RenderField}
-        type="text"
-        label={t("userEdit.form.field.about")}
-        value={profile.about}
-      />
-
-      <Field
-        name="profile.designation"
-        component={RenderField}
-        type="text"
-        label={t("userEdit.form.field.designation")}
-        value={profile.designation}
-      />
-
-      <Field
-        name="profile.mobile"
-        component={RenderField}
-        type="text"
-        label={t("userEdit.form.field.mobile")}
-        value={profile.mobile}
-      />
-
-      {isAdmin && (
+          name="profile.avatar"
+          component={RenderUpload}
+          type="text"
+          label={t('userEdit.form.field.avatar')}
+          value={profile.avatar}
+        />
+        <div className="g2">
         <Field
-          name="profile.flag"
+          name="username"
           component={RenderField}
           type="text"
-          label={t("userEdit.form.field.flag")}
-          value={profile.flag}
+          label={t('userEdit.form.field.name')}
+          value={username}
         />
-      )}
+        <Field name="email" component={RenderField} type="email" label={t('userEdit.form.field.email')} value={email} />
+        {isAdmin && (
+          <Field name="role" component={RenderSelect} label={t('userEdit.form.field.role.label')} value={role}>
+            <Option value="user">{t('userEdit.form.field.role.user')}</Option>
+            <Option value="admin">{t('userEdit.form.field.role.admin')}</Option>
+          </Field>
+        )}
 
-      {isAdmin && (
         <Field
-          name="profile.rating"
+          name="profile.firstName"
           component={RenderField}
           type="text"
-          label={t("userEdit.form.field.rating")}
-          value={profile.rating}
+          label={t('userEdit.form.field.firstName')}
+          value={profile.firstName}
         />
-      )}
-
-      <Field
-        name="profile.website"
-        component={RenderField}
-        type="text"
-        label={t("userEdit.form.field.website")}
-        value={profile.website}
-      />
-
-      <Field
-        name="profile.isAvailable"
-        component={RenderCheckBox}
-        type="checkbox"
-        label={t("userEdit.form.field.available")}
-        checked={profile.isAvailable}
-      />
-
-      {isAdmin && (
         <Field
-          name="isActive"
-          component={RenderCheckBox}
-          type="checkbox"
-          label={t("userEdit.form.field.active")}
-          checked={isActive}
+          name="profile.lastName"
+          component={RenderField}
+          type="text"
+          label={t('userEdit.form.field.lastName')}
+          value={profile.lastName}
         />
-      )}
 
-      {isAdmin && (
         <Field
-          name="profile.isVerified"
-          component={RenderCheckBox}
-          type="checkbox"
-          label={t("userEdit.form.field.isverified")}
-          checked={profile.isVerified}
+          name="profile.about"
+          component={RenderField}
+          type="text"
+          label={t('userEdit.form.field.about')}
+          value={profile.about}
         />
-      )}
 
-      <FieldArray
-        name="addresses"
-        render={arrayHelpers => (
-          <RenderDynamicField
-            keys={[
-              { key: "streetAddress1", type: "text" },
-              { key: "streetAddress2", type: "text" },
-              { key: "city", type: "text" },
-              { key: "state", type: "text" },
-              { key: "pinCode", type: "text" }
-            ]}
-            buttonText="Add Address"
-            style={{width:'40%'}}
-            arrayHelpers={arrayHelpers}
-            values={addresses}
-            name="addresses"
-            label={t("userEdit.form.field.addresses")}
+        <Field
+          name="profile.designation"
+          component={RenderField}
+          type="text"
+          label={t('userEdit.form.field.designation')}
+          value={profile.designation}
+        />
+
+        <Field
+          name="profile.mobile"
+          component={RenderField}
+          type="text"
+          label={t('userEdit.form.field.mobile')}
+          value={profile.mobile}
+        />
+
+        {isAdmin && (
+          <Field
+            name="profile.flag"
+            component={RenderField}
+            type="text"
+            label={t('userEdit.form.field.flag')}
+            value={profile.flag}
           />
         )}
-      />
 
-      <FieldArray
-        name="portfolios"
-        render={arrayHelpers => (
-          <RenderDynamicField
-            keys={[
-              { key: "platform", type: "text" },
-              { key: "portfolioUrl", type: "text" }
-            ]}
-            buttonText="Add Portfolio"
-            style={{width:'40% !important'}}
-            arrayHelpers={arrayHelpers}
-            values={portfolios}
-            name="portfolios"
-            label={t("userEdit.form.field.portfolios")}
+        {isAdmin && (
+          <Field
+            name="profile.rating"
+            component={RenderField}
+            type="text"
+            label={t('userEdit.form.field.rating')}
+            value={profile.rating}
           />
         )}
-      />
 
-      {settings.auth.certificate.enabled && (
         <Field
-          name="serial"
+          name="profile.website"
           component={RenderField}
           type="text"
-          label={t("userEdit.form.field.serial")}
-          value={auth && auth.certificate && auth.certificate.serial}
-          onChange={value =>
-            setFieldValue("auth", {
-              ...auth,
-              certificate: { ...auth.certificate, serial: value }
-            })
-          }
+          label={t('userEdit.form.field.website')}
+          value={profile.website}
         />
-      )}
-      {isAdmin && (
-        <div>
+        <div></div>
+        <Field
+          name="profile.isAvailable"
+          component={RenderCheckBox}
+          type="checkbox"
+          label={t('userEdit.form.field.available')}
+          checked={profile.isAvailable}
+        />
+
+        {isAdmin && (
           <Field
-            name="password"
-            component={RenderField}
-            type="password"
-            label={t("userEdit.form.field.pass")}
-            value={password}
+            name="isActive"
+            component={RenderCheckBox}
+            type="checkbox"
+            label={t('userEdit.form.field.active')}
+            checked={isActive}
           />
+        )}
+
+        {isAdmin && (
           <Field
-            name="passwordConfirmation"
-            component={RenderField}
-            type="password"
-            label={t("userEdit.form.field.passConf")}
-            value={passwordConfirmation}
+            name="profile.isVerified"
+            component={RenderCheckBox}
+            type="checkbox"
+            label={t('userEdit.form.field.isverified')}
+            checked={profile.isVerified}
           />
+        )}
         </div>
-      )}
-      {errors && errors.errorMsg && (
-        <Alert color="error">{errors.errorMsg}</Alert>
-      )}
-      <Button color="primary" type="submit">
-        {t("userEdit.form.btnSubmit")}
-      </Button>
-    </Form>
-  </div>
+          <br/>
+          <div className="g3">
+
+
+        <FieldArray
+          name="addresses"
+          render={arrayHelpers => (
+            <RenderDynamicField
+              keys={[
+                { key: 'streetAddress1', type: 'text' },
+                { key: 'streetAddress2', type: 'text' },
+                { key: 'city', type: 'text' },
+                { key: 'state', type: 'text' },
+                { key: 'pinCode', type: 'text' }
+              ]}
+              buttonText="Add Address"
+              style={{ width: '40%' }}
+              arrayHelpers={arrayHelpers}
+              values={addresses}
+              name="addresses"
+              label={t('userEdit.form.field.addresses')}
+            />
+          )}
+        />
+
+        <FieldArray
+          name="portfolios"
+          render={arrayHelpers => (
+            <RenderDynamicField
+              keys={[{ key: 'platform', type: 'text' }, { key: 'portfolioUrl', type: 'text' }]}
+              buttonText="Add Portfolio"
+              style={{ width: '40% !important' }}
+              arrayHelpers={arrayHelpers}
+              values={portfolios}
+              name="portfolios"
+              label={t('userEdit.form.field.portfolios')}
+            />
+          )}
+        />
+
+        {settings.auth.certificate.enabled && (
+          <Field
+            name="serial"
+            component={RenderField}
+            type="text"
+            label={t('userEdit.form.field.serial')}
+            value={auth && auth.certificate && auth.certificate.serial}
+            onChange={value =>
+              setFieldValue('auth', {
+                ...auth,
+                certificate: { ...auth.certificate, serial: value }
+              })
+            }
+          />
+        )}
+        {isAdmin && (
+          <div>
+            <Field
+              name="password"
+              component={RenderField}
+              type="password"
+              label={t('userEdit.form.field.pass')}
+              value={password}
+            />
+            <Field
+              name="passwordConfirmation"
+              component={RenderField}
+              type="password"
+              label={t('userEdit.form.field.passConf')}
+              value={passwordConfirmation}
+            />
+          </div>
+        )}
+        {errors && errors.errorMsg && <Alert color="error">{errors.errorMsg}</Alert>}
+        <Button color="primary" type="submit">
+          {t('userEdit.form.btnSubmit')}
+        </Button>
+        
+        </div>
+      </Form>
+    </div>
   );
 };
 
@@ -315,15 +285,7 @@ UserForm.propTypes = {
 
 const UserFormWithFormik = withFormik({
   mapPropsToValues: values => {
-    const {
-      username,
-      email,
-      role,
-      isActive,
-      profile,
-      addresses,
-      portfolios
-    } = values.initialValues;
+    const { username, email, role, isActive, profile, addresses, portfolios } = values.initialValues;
 
     const userRole = values.userRole;
 
@@ -348,10 +310,10 @@ const UserFormWithFormik = withFormik({
       userRole: userRole,
       username: username,
       email: email,
-      role: role || "user",
+      role: role || 'user',
       isActive: isActive,
-      password: "",
-      passwordConfirmation: "",
+      password: '',
+      passwordConfirmation: '',
       profile: {
         firstName: profile && profile.firstName,
         lastName: profile && profile.lastName,
@@ -365,12 +327,8 @@ const UserFormWithFormik = withFormik({
         rating: profile && profile.rating,
         website: profile && profile.website
       },
-      addresses:
-        addresses && addresses.length !== 0 ? addresses.map(getAddresses) : [],
-      portfolios:
-        portfolios && portfolios.length !== 0
-          ? portfolios.map(getPortfolios)
-          : [],
+      addresses: addresses && addresses.length !== 0 ? addresses.map(getAddresses) : [],
+      portfolios: portfolios && portfolios.length !== 0 ? portfolios.map(getPortfolios) : [],
       auth: {
         ...values.initialValues.auth
       }
@@ -391,12 +349,9 @@ const UserFormWithFormik = withFormik({
       }
     });
   },
-  displayName: "SignUpForm ", // helps with React DevTools
+  displayName: 'SignUpForm ', // helps with React DevTools
   validate: (values, props) =>
-    validate(
-      values,
-      isEmpty(props.initialValues) ? createUserFormSchema : updateUserFormSchema
-    )
+    validate(values, isEmpty(props.initialValues) ? createUserFormSchema : updateUserFormSchema)
 });
 
-export default translate("user")(UserFormWithFormik(UserForm));
+export default translate('user')(UserFormWithFormik(UserForm));
