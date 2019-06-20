7e049652ee99c4622944e5ac8c524550c7e28c4e
Bishal Deb <bishaldeb656@gmail.com>
2019-06-20 17:10:43 +0530

    Owner details section added in List Your Gear form.

---
+++ modules/user/client-react/components/UserDetailsView.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { translate } from "@gqlapp/i18n-client-react";
+
+import UserForm from "./UserForm";
+
+const UserEditView = ({ loading, user, t, currentUser, onSubmit }) => {
+  const isNotSelf = !user || (user && user.id !== currentUser.id);
+  return (
+    <div>
+      {loading && !user ? (
+        <div className="text-center">{t("userEdit.loadMsg")}</div>
+      ) : (
+        <UserForm
+          onSubmit={onSubmit}
+          shouldDisplayRole={isNotSelf}
+          shouldDisplayActive={isNotSelf}
+          initialValues={user}
+          userRole={currentUser && currentUser.role ? currentUser.role : null}
+        />
+      )}
+    </div>
+  );
+};
+
+UserEditView.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  user: PropTypes.object,
+  currentUser: PropTypes.object,
+  t: PropTypes.func,
+  onSubmit: PropTypes.func
+};
+
+export default translate("user")(UserEditView);
