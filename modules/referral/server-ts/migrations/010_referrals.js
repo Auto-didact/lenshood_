a706a85130e94ffbb9a133ef763575f431f36c46
Bishal Deb <bishaldeb656@gmail.com>
2019-07-01 04:45:01 +0530

    column for is_verified added in referral

--- modules/referral/server-ts/migrations/010_referrals.js
+++ modules/referral/server-ts/migrations/010_referrals.js
@@ -14,6 +14,7 @@ exports.up = function(knex, Promise) {
         .references("id")
         .inTable("user")
         .onDelete("CASCADE");
+      table.boolean("is_verified").defaultTo(false);
       table.timestamps(false, true);
     })
   ]);
