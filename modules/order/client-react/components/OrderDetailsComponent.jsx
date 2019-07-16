81268642fbec1ddf17d6affc1f6541335aad6beb
r-kohale9 <r.kohale9@gmail.com>
2019-07-17 02:27:31 +0530

    styling for my-orders

--- modules/order/client-react/components/OrderDetailsComponent.jsx
+++ modules/order/client-react/components/OrderDetailsComponent.jsx
@@ -9,125 +9,86 @@ class OrderDetailsComponent extends Component {
 
     return (
       <Card
-        className="boxShadowTheme borderRadius9 marginT20"
+        className="orderCard"
         hoverable
         bodyStyle={{
           padding: '0px'
         }}
       >
-        {/* <Row type="flex" justify="space-around" align="middle">
-          <Col xs={{ span: 24 }} md={{ span: 9 }} xxl={{ span: 6 }} className="DetailsCardCol" align="center">
+        <Row type="flex" justify="space-around" align="middle">
+          <Col xs={{ span: 24 }} md={{ span: 9 }} xxl={{ span: 6 }} className="orderCardCol" align="center">
             <img className="orderImage" alt="" src={item.image.length !== 0 ? item.image : ImgCamera[0].imageUrl} />
           </Col>
-          <Col xs={{ span: 24 }} md={{ span: 15 }} xxl={{ span: 18 }} className="DetailsCardCol">
+          <Col xs={{ span: 24 }} md={{ span: 15 }} xxl={{ span: 18 }} className="orderCardCol">
             <div style={{ padding: '10px', align: 'center' }}>
-              <Button type="primary" ghost size="small" block onClick={e => this.props.setTrackList(item.id)}>
+              <Button
+                className="trackTag font12 borderRadius9"
+                style={{
+                  width: '15%',
+                  position: 'relative',
+                  left: '5px',
+                  bottom: '5px'
+                }}
+                type="primary"
+                ghost
+                size="small"
+                block
+                onClick={() => this.props.setTrackList(item.id)}
+              >
                 <Icon type="environment" theme="filled" /> Track
               </Button>
-              <h3 className="itemName">{item.name ? <span>{item.name}</span> : 'Info Not Provided'}</h3>
-
-
-              <Row style={{ marginBottom: '8px' }}>
-                <Col span={12} align="left" style={{ paddingLeft: '20px' }}>
-                  <Link to={`/listing/${item.id}`}>
-                    <Button shape="circle" size="large">
-                      <Icon type="edit" />
-                    </Button>
-                  </Link>
-                </Col>
-                <Col span={12} align="right" style={{ paddingRight: '20px' }}>
-                  <Popconfirm
-                    title="Are you sure to delete this listing?"
-                    onConfirm={e => this.props.DeleteListing(item.id, e)}
-                    onCancel={cancel}
-                    okText="Yes"
-                    cancelText="No"
-                  >
-                    <Button type="danger" shape="circle" size="large">
-                      <Icon type="delete" />
-                    </Button>
-                  </Popconfirm>
-                </Col>
-              </Row>
-              <Row gutter={13} align="middle">
-                <Col span={12}>
-                  <Button type="primary" block onClick={e => this.props.toggle(item.id, e)}>
-                    {item.status === ONSHELF ? 'Move to Idle' : 'Move to Shelf'}
-                  </Button>
-                </Col>
-                <Col span={12}>
-                  <Button type="primary" block>
-                    View Listing
-                  </Button>
-                </Col>
-              </Row>
-            </div>
-          </Col>
-        </Row>
- */}
-        <Row>
-          <Col span={24} className="marginB10 PadA10">
-            <Col sm={19} xs={24}>
-              <span className="itemName">{item.id}</span>
-            </Col>
-            <Col sm={5} xs={12}>
-              <Button type="primary" ghost size="small" block onClick={e => this.props.setTrackList(item.id)}>
-                <Icon type="environment" theme="filled" /> Track
-              </Button>
-            </Col>
-          </Col>
-          <br />
-          <Col md={7} sm={9} xs={24} style={{ paddingLeft: '52px' }}>
-            <img height={140} src={item.image} alt="" className="width100" />
-          </Col>
-
-          <Col md={17} sm={15} xs={24} className="PadA10">
-            <h4 className="itemName">{item.name}</h4>
-
-            <Col sm={14} xs={24}>
-              <h4 className="itemDetails">
+              <h2 className="itemName">
+                {item.name ? (
+                  <span>
+                    <b>{item.name}</b>
+                  </span>
+                ) : (
+                  'Info Not Provided'
+                )}
+              </h2>
+              <h3 className="itemDetails">
                 {item.details}{' '}
                 <span className="colorcursor">
                   <Icon type="car" theme="filled" />
                 </span>
-              </h4>
+              </h3>
               <p className="orderstartend">
                 {item.date.start} - {item.date.end}
               </p>
-              <Col sm={5} xs={6}>
-                <Avatar src={item.userimg} />
-              </Col>
-              <Col sm={19} xs={18}>
-                <h4 className="font12">
-                  {item.seller}
-                  <br />
-                  <p className="font10 mainColor">
-                    {' '}
-                    <Rate disabled defaultValue={item.stars} className="font10 mainColor" />
-                  </p>
+              <Avatar style={{ position: 'relative', left: '20px' }} src={item.userimg} />
+              <h3 className="font12">
+                {item.seller}
+                <p className="font10 mainColor">
+                  <Rate disabled defaultValue={item.stars} className="font10 mainColor" />
+                </p>
+              </h3>
+              <Col span={24} className="orderTotalDate">
+                <h4>
+                  <Col sm={17} xs={24} className="orderGrey">
+                    <span>{item.status === BORROWED ? 'Ordered' : 'Lended'} on</span> {item.orderDate}{' '}
+                  </Col>
+                  <Col sm={7} xs={24}>
+                    <span className="orderGrey">
+                      <strong>
+                        <span>{item.status === BORROWED ? 'Order' : 'Lend'} Total</span> &#8377; {item.orderTotal}
+                      </strong>
+                    </span>
+                  </Col>
                 </h4>
               </Col>
-            </Col>
-            <Col sm={10} xs={24}>
-              <Button type="primary" size="small" className="CancelRequest font12" block>
-                Cancel booking
-              </Button>
-              <Button type="primary" className="CancelRequest font12" size="small" block>
-                Request Extension
-              </Button>
-            </Col>
-          </Col>
-          <Col span={24} className="orderTotalDate">
-            <h4>
-              <Col sm={17} xs={24}>
-                <span className="orderGrey">{item.status === BORROWED ? 'Ordered' : 'Lended'} on</span> {item.orderDate}{' '}
-              </Col>
-              <Col sm={7} xs={24}>
-                <span className="orderGrey">
-                  <span>{item.status === BORROWED ? 'Order' : 'Lend'} Total</span> &#8377; {item.orderTotal}
-                </span>
-              </Col>
-            </h4>
+              <Row gutter={13} align="middle">
+                <Col span={12}>
+                  <Button type="primary" block>
+                    Cancel booking
+                  </Button>
+                </Col>
+                <Col span={12}>
+                  <Button type="primary" block>
+                    Request Extension
+                  </Button>
+                </Col>
+              </Row>
+            </div>
           </Col>
         </Row>
       </Card>
