41df5e71ef94cc4151b28b4b8303485a5a9f7f88
Mithilesh Kohale <beingtmk@gmail.com>
2019-07-17 16:13:49 +0530

    remove duplicate declaration

--- modules/listing/server-ts/sql.ts
+++ modules/listing/server-ts/sql.ts
@@ -720,46 +720,6 @@ class ListingWatchListDAO extends Model {
   static get relationMappings() {
     return {
       listing: {
-        relation: Model.HasManyRelation,
-        modelClass: ListingDAO,
-        join: {
-          from: 'listing_review.listing_id',
-          to: 'listing.id'
-        }
-      },
-      reviewer: {
-        relation: Model.HasManyRelation,
-        modelClass: UserProfile,
-        join: {
-          from: 'listing_review.reviewer_id',
-          to: 'user_profile.user_id'
-        }
-      },
-      likedislikes: {
-        relation: Model.HasManyRelation,
-        modelClass: UserReviewLikesDAO,
-        join: {
-          from: 'listing_review.id',
-          to: 'user_reviews_likes.listing_review_id'
-        }
-      }
-    };
-  }
-}
-
-// ListingWatchListDAO model.
-class ListingWatchListDAO extends Model {
-  static get tableName() {
-    return 'watchlist';
-  }
-
-  static get idColumn() {
-    return 'id';
-  }
-
-  static get relationMappings() {
-    return {
-      listing: {
         relation: Model.BelongsToOneRelation,
         modelClass: ListingDAO,
         join: {
