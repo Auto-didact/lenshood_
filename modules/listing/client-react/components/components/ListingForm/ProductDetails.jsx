f66b542ad1eb07f7469a385a7b3491a00e9d785c
r-kohale9 <r.kohale9@gmail.com>
2019-07-18 23:02:16 +0530

    resolved merge conflicts

--- modules/listing/client-react/components/components/ListingForm/ProductDetails.jsx
+++ modules/listing/client-react/components/components/ListingForm/ProductDetails.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { FieldAdapter as Field } from "@gqlapp/forms-client-react";
-import { FieldArray } from "formik";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { FieldAdapter as Field } from '@gqlapp/forms-client-react';
+import { FieldArray } from 'formik';
 import {
   RenderField,
   RenderSelect,
@@ -11,10 +11,10 @@ import {
   RenderDynamicField,
   RenderCheckBox,
   RenderRadioGroup
-} from "@gqlapp/look-client-react";
+} from '@gqlapp/look-client-react';
 
 // Abstract Radio Button out To Do
-import { Radio, Select } from "antd";
+import { Radio, Select } from 'antd';
 
 const RadioButton = Radio.Button;
 
@@ -24,54 +24,26 @@ export default class ProductDetails extends Component {
     this.state = {
       /* To Do - Find a way to handle these options in a better way (Database or Constants) */
       listingCategories: {
-        gearCategory: [
-          "Cameras",
-          "Lenses",
-          "Tripods and Support",
-          "Lighting",
-          "Studio Space"
-        ],
+        gearCategory: ['Cameras', 'Lenses', 'Tripods and Support', 'Lighting', 'Studio Space'],
         gearSubcategory: {
-          Cameras: [
-            "DSLR's",
-            "Point and Shoot cameras",
-            "Mirrorless cameras",
-            "Action cams",
-            "Cinema Cameras"
-          ],
-          Lenses: ["DSLR Lenses", "Mirrorless Lenses", "Cinema Lenses"],
-          Lighting: [
-            "Photography Lighting",
-            "Video Lighting",
-            "Lighting Accesories"
-          ],
-          "Tripods and Support": [
-            "Tripods/Monopods",
-            "Gimbals/Stabilizers",
-            "Rigs/Sliders"
-          ]
+          Cameras: ["DSLR's", 'Point and Shoot cameras', 'Mirrorless cameras', 'Action cams', 'Cinema Cameras'],
+          Lenses: ['DSLR Lenses', 'Mirrorless Lenses', 'Cinema Lenses'],
+          Lighting: ['Photography Lighting', 'Video Lighting', 'Lighting Accesories'],
+          'Tripods and Support': ['Tripods/Monopods', 'Gimbals/Stabilizers', 'Rigs/Sliders']
         },
         gearSubSubcategory: {
-          "DSLR Lenses": [
-            "Standard/Zoom Lens",
-            "Wide angle/Fish eye Lens",
-            "Fixed/Prime Lens"
-          ],
-          "Mirrorless Lenses": [
-            "Standard/Zoom Lens",
-            "Wide angle/Fish eye Lens",
-            "Fixed/Prime Lens"
-          ]
+          'DSLR Lenses': ['Standard/Zoom Lens', 'Wide angle/Fish eye Lens', 'Fixed/Prime Lens'],
+          'Mirrorless Lenses': ['Standard/Zoom Lens', 'Wide angle/Fish eye Lens', 'Fixed/Prime Lens']
         }
       },
 
-      status: ["idle", "on_rent", "on_shelf", "disabled"],
+      status: ['idle', 'on_rent', 'on_shelf', 'disabled'],
       activeGearCategory: this.props.values.gearCategory,
       activeGearSubcategories: this.props.values.gearSubcategory,
       activeGearSubSubcategory: this.props.values.gearSubSubcategory,
 
-      listingCondition: ["New", "Good", "Fair"],
-      listingAge: ["< 1 year", "1 - 2 years", "> 3 years"],
+      listingCondition: ['New', 'Good', 'Fair'],
+      listingAge: ['< 1 year', '1 - 2 years', '> 3 years'],
 
       listingContent: props.values.listingContent,
 
@@ -81,12 +53,8 @@ export default class ProductDetails extends Component {
       dataSource: []
     };
     this.handleGearCategoryChange = this.handleGearCategoryChange.bind(this);
-    this.handleGearSubSubcategoryChange = this.handleGearSubSubcategoryChange.bind(
-      this
-    );
-    this.handleGearSubCategoryChange = this.handleGearSubCategoryChange.bind(
-      this
-    );
+    this.handleGearSubSubcategoryChange = this.handleGearSubSubcategoryChange.bind(this);
+    this.handleGearSubCategoryChange = this.handleGearSubCategoryChange.bind(this);
 
     // FOR RENDERAUTOCOMPLETE
     this.handleSearch = this.handleSearch.bind(this);
@@ -140,14 +108,8 @@ export default class ProductDetails extends Component {
       item =>
         item.username.toUpperCase().includes(query.toUpperCase()) ||
         (item.profile &&
-          ((item.profile.firstName &&
-            item.profile.firstName
-              .toUpperCase()
-              .includes(query.toUpperCase())) ||
-            (item.profile.lastName &&
-              item.profile.lastName
-                .toUpperCase()
-                .includes(query.toUpperCase()))))
+          ((item.profile.firstName && item.profile.firstName.toUpperCase().includes(query.toUpperCase())) ||
+            (item.profile.lastName && item.profile.lastName.toUpperCase().includes(query.toUpperCase()))))
     );
     return items;
   }
@@ -165,7 +127,7 @@ export default class ProductDetails extends Component {
               name="username"
               dataSource={this.state.dataSource.map(item => item.username)}
               component={RenderAutoComplete}
-              label={t("listing.field.username")}
+              label={t('listing.field.username')}
               value={values.username}
               // onSelect={this.onSelect}
               onSearch={this.handleSearch}
@@ -178,7 +140,7 @@ export default class ProductDetails extends Component {
           name="gearCategory"
           component={RenderSelect}
           type="text"
-          label={t("listing.field.gearCategory")}
+          label={t('listing.field.gearCategory')}
           value={this.state.activeGearCategory}
           onChange={this.handleGearCategoryChange}
         >
@@ -190,20 +152,16 @@ export default class ProductDetails extends Component {
         </Field>
 
         {/* Gear Sub-Category */}
-        {this.state.listingCategories.gearSubcategory[
-          this.state.activeGearCategory
-        ] ? (
+        {this.state.listingCategories.gearSubcategory[this.state.activeGearCategory] ? (
           <Field
             name="gearSubcategory"
             component={RenderSelect}
             type="text"
-            label={t("listing.field.gearSubcategory")}
+            label={t('listing.field.gearSubcategory')}
             value={this.state.activeGearSubcategories}
             onChange={this.handleGearSubCategoryChange}
           >
-            {this.state.listingCategories.gearSubcategory[
-              this.state.activeGearCategory
-            ].map((category, idx) => (
+            {this.state.listingCategories.gearSubcategory[this.state.activeGearCategory].map((category, idx) => (
               <Select.Option key={idx} value={category}>
                 {category}
               </Select.Option>
@@ -211,43 +169,38 @@ export default class ProductDetails extends Component {
           </Field>
         ) : null}
 
-        {this.state.listingCategories.gearSubSubcategory[
-          this.state.activeGearSubcategories
-        ] ? (
+        {this.state.listingCategories.gearSubSubcategory[this.state.activeGearSubcategories] ? (
           <>
-            {this.state.listingCategories.gearSubSubcategory[
-              this.state.activeGearSubcategories
-            ].some(
-              item =>
-                item == "Standard/Zoom Lens" ||
-                item == "Wide angle/Fish eye Lens"
+            {this.state.listingCategories.gearSubSubcategory[this.state.activeGearSubcategories].some(
+              item => item == 'Standard/Zoom Lens' || item == 'Wide angle/Fish eye Lens'
             ) ? (
               <p>
-              <strong>Note: </strong>
-              <ul>
-                <li>
-                  <strong>Wide angle/fisheye</strong> - below 18mm
-                </li>
-                <li>
-                  <strong>zoom/standard</strong> - above 18mm
-                </li>
-              </ul></p>
+                <strong>Note: </strong>
+                <ul>
+                  <li>
+                    <strong>Wide angle/fisheye</strong> - below 18mm
+                  </li>
+                  <li>
+                    <strong>zoom/standard</strong> - above 18mm
+                  </li>
+                </ul>
+              </p>
             ) : null}
             <Field
               name="gearSubSubcategory"
               component={RenderSelect}
               type="text"
-              label={t("listing.field.gearSubSubcategory")}
+              label={t('listing.field.gearSubSubcategory')}
               value={this.state.activeGearSubSubcategory}
               onChange={this.handleGearSubSubcategoryChange}
             >
-              {this.state.listingCategories.gearSubSubcategory[
-                this.state.activeGearSubcategories
-              ].map((category, idx) => (
-                <Select.Option key={idx} value={category}>
-                  {category}
-                </Select.Option>
-              ))}
+              {this.state.listingCategories.gearSubSubcategory[this.state.activeGearSubcategories].map(
+                (category, idx) => (
+                  <Select.Option key={idx} value={category}>
+                    {category}
+                  </Select.Option>
+                )
+              )}
             </Field>
           </>
         ) : null}
@@ -257,7 +210,7 @@ export default class ProductDetails extends Component {
             name="status"
             component={RenderSelect}
             type="select"
-            label={t("listing.field.status")}
+            label={t('listing.field.status')}
             value={values.status}
           >
             {this.state.status.map((status, idx) => (
@@ -273,7 +226,7 @@ export default class ProductDetails extends Component {
           <Field
             name="isActive"
             component={RenderCheckBox}
-            label={t("listing.field.isActive")}
+            label={t('listing.field.isActive')}
             checked={values.isActive}
           />
         ) : null}
@@ -286,30 +239,30 @@ export default class ProductDetails extends Component {
               buttonText="Add Component"
               keys={[
                 {
-                  key: "gear",
-                  type: "text",
-                  label: "Component Type",
-                  placeholder: "ex: Body, Lens"
+                  key: 'gear',
+                  type: 'text',
+                  label: 'Component Type',
+                  placeholder: 'ex: Body, Lens'
                 },
                 {
-                  key: "brand",
-                  type: "text",
-                  label: "Brand",
-                  placeholder: "ex: Canon, NIkon"
+                  key: 'brand',
+                  type: 'text',
+                  label: 'Brand',
+                  placeholder: 'ex: Canon, NIkon'
                 },
                 {
-                  key: "model",
-                  type: "text",
-                  label: "Model",
-                  placeholder: "ex: 5D MARK IV, D5600"
+                  key: 'model',
+                  type: 'text',
+                  label: 'Model',
+                  placeholder: 'ex: 5D MARK IV, D5600'
                 },
-                { key: "serial", type: "text", label: "Serial Number" }
+                { key: 'serial', type: 'text', label: 'Serial Number' }
               ]}
               arrayHelpers={arrayHelpers}
               values={values.listingContent}
               initialValues={this.state.listingContent}
               name="listingContent"
-              label={t("listing.field.gearComponents")}
+              label={t('listing.field.gearComponents')}
             />
           )}
         />
@@ -318,13 +271,9 @@ export default class ProductDetails extends Component {
         {/* Listing Images */}
         <FieldArray
           name="listingImages"
-          label={t("listing.field.listingImages")}
+          label={t('listing.field.listingImages')}
           render={arrayHelpers => (
-            <RenderUploadMultiple
-              arrayHelpers={arrayHelpers}
-              values={values.listingImages}
-              dictKey="imageUrl"
-            />
+            <RenderUploadMultiple arrayHelpers={arrayHelpers} values={values.listingImages} dictKey="imageUrl" />
           )}
         />
 
@@ -333,7 +282,7 @@ export default class ProductDetails extends Component {
           name="description"
           component={RenderField}
           type="textarea"
-          label={t("listing.field.description")}
+          label={t('listing.field.description')}
           value={values.description}
         />
 
@@ -342,7 +291,7 @@ export default class ProductDetails extends Component {
             name="listingDetail.condition"
             component={RenderRadioGroup}
             type="text"
-            label={t("listing.field.listingDetail.condition")}
+            label={t('listing.field.listingDetail.condition')}
             value={values.listingDetail.condition}
           >
             {this.state.listingCondition.map((condition, idx) => (
@@ -357,7 +306,7 @@ export default class ProductDetails extends Component {
           name="listingDetail.age"
           component={RenderRadioGroup}
           type="text"
-          label={t("listing.field.listingDetail.age")}
+          label={t('listing.field.listingDetail.age')}
           value={values.listingDetail.age}
         >
           {this.state.listingAge.map((value, idx) => (
@@ -373,13 +322,13 @@ export default class ProductDetails extends Component {
             <RenderDynamicField
               buttonText="Add Damages"
               keys={[
-                { key: "imageUrl", type: "image", label: "Image" },
-                { key: "damageDetail", type: "text", label: "Details" }
+                { key: 'imageUrl', type: 'image', label: 'Image' },
+                { key: 'damageDetail', type: 'text', label: 'Details' }
               ]}
               arrayHelpers={arrayHelpers}
               values={values.listingDetail.damages}
               name="listingDetail.damages"
-              label={t("listing.field.listingDamages")}
+              label={t('listing.field.listingDamages')}
             />
           )}
         />
