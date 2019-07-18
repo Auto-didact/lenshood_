952f187acc53a4948e8b3a45053d1c71920baf79
Bishal Deb <bishaldeb656@gmail.com>
2019-07-18 17:54:11 +0530

    gearSubSubcategory added in sql, resolver and schema.

--- modules/listing/server-ts/migrations/008_listing.js
+++ modules/listing/server-ts/migrations/008_listing.js
@@ -1,143 +1,144 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema
-      .createTable('listing', table => {
-        table.increments('id');
+      .createTable("listing", table => {
+        table.increments("id");
         table
-          .integer('user_id')
+          .integer("user_id")
           .unsigned()
-          .references('id')
-          .inTable('user')
-          .onDelete('CASCADE');
-        table.string('gear_category');
-        table.string('gear_subcategory');
-        table.string('description');
-        table.string('status').defaultTo('Idle');
-        table.boolean('is_active').defaultTo(true);
-        table.boolean('is_featured').defaultTo(false);
+          .references("id")
+          .inTable("user")
+          .onDelete("CASCADE");
+        table.string("gear_category");
+        table.string("gear_subcategory");
+        table.string("gear_sub_subcategory");
+        table.string("description");
+        table.string("status").defaultTo("Idle");
+        table.boolean("is_active").defaultTo(true);
+        table.boolean("is_featured").defaultTo(false);
         table.timestamps(false, true);
       })
-      .createTable('listing_image', table => {
-        table.increments('id');
+      .createTable("listing_image", table => {
+        table.increments("id");
         table
-          .integer('listing_id')
+          .integer("listing_id")
           .unsigned()
-          .references('id')
-          .inTable('listing')
-          .onDelete('CASCADE');
-        table.string('image_url');
+          .references("id")
+          .inTable("listing")
+          .onDelete("CASCADE");
+        table.string("image_url");
         table.timestamps(false, true);
       })
-      .createTable('listing_detail', table => {
-        table.increments('id');
+      .createTable("listing_detail", table => {
+        table.increments("id");
         table
-          .integer('listing_id')
+          .integer("listing_id")
           .unsigned()
-          .references('id')
-          .inTable('listing')
-          .onDelete('CASCADE');
-        table.string('condition');
-        table.string('age');
+          .references("id")
+          .inTable("listing")
+          .onDelete("CASCADE");
+        table.string("condition");
+        table.string("age");
         table.timestamps(false, true);
       })
-      .createTable('listing_damage', table => {
-        table.increments('id');
+      .createTable("listing_damage", table => {
+        table.increments("id");
         table
-          .integer('listing_detail_id')
+          .integer("listing_detail_id")
           .unsigned()
-          .references('id')
-          .inTable('listing_detail')
-          .onDelete('CASCADE');
-        table.string('image_url');
-        table.string('damage_detail');
+          .references("id")
+          .inTable("listing_detail")
+          .onDelete("CASCADE");
+        table.string("image_url");
+        table.string("damage_detail");
         table.timestamps(false, true);
       })
-      .createTable('listing_rental', table => {
-        table.increments('id');
+      .createTable("listing_rental", table => {
+        table.increments("id");
         table
-          .integer('listing_id')
+          .integer("listing_id")
           .unsigned()
-          .references('id')
-          .inTable('listing')
-          .onDelete('CASCADE');
-        table.integer('per_day');
-        table.integer('per_week');
-        table.integer('per_month');
-        table.integer('replacement_value');
+          .references("id")
+          .inTable("listing")
+          .onDelete("CASCADE");
+        table.integer("per_day");
+        table.integer("per_week");
+        table.integer("per_month");
+        table.integer("replacement_value");
         table.timestamps(false, true);
       })
-      .createTable('listing_content', table => {
-        table.increments('id');
+      .createTable("listing_content", table => {
+        table.increments("id");
         table
-          .integer('listing_id')
+          .integer("listing_id")
           .unsigned()
-          .references('id')
-          .inTable('listing')
-          .onDelete('CASCADE');
-        table.string('gear');
-        table.string('brand');
-        table.string('model');
-        table.string('serial');
+          .references("id")
+          .inTable("listing")
+          .onDelete("CASCADE");
+        table.string("gear");
+        table.string("brand");
+        table.string("model");
+        table.string("serial");
         table.timestamps(false, true);
       })
-      .createTable('listing_review', table => {
-        table.increments('id');
+      .createTable("listing_review", table => {
+        table.increments("id");
         table
-          .integer('listing_id')
+          .integer("listing_id")
           .unsigned()
-          .references('id')
-          .inTable('listing')
-          .onDelete('CASCADE');
+          .references("id")
+          .inTable("listing")
+          .onDelete("CASCADE");
         table
-          .integer('reviewer_id')
+          .integer("reviewer_id")
           .unsigned()
-          .references('id')
-          .inTable('user')
-          .onDelete('CASCADE');
-        table.string('comment');
-        table.string('rating');
-        table.boolean('is_reply').defaultTo(false);
+          .references("id")
+          .inTable("user")
+          .onDelete("CASCADE");
+        table.string("comment");
+        table.string("rating");
+        table.boolean("is_reply").defaultTo(false);
         table.timestamps(false, true);
       })
-      .createTable('watchlist', table => {
-        table.increments('id');
+      .createTable("watchlist", table => {
+        table.increments("id");
         table
-          .integer('user_id')
+          .integer("user_id")
           .unsigned()
-          .references('id')
-          .inTable('user')
-          .onDelete('CASCADE');
+          .references("id")
+          .inTable("user")
+          .onDelete("CASCADE");
         table
-          .integer('listing_id')
+          .integer("listing_id")
           .unsigned()
-          .references('id')
-          .inTable('listing')
-          .onDelete('CASCADE');
-        table.string('should_notify').defaultTo(true);
+          .references("id")
+          .inTable("listing")
+          .onDelete("CASCADE");
+        table.string("should_notify").defaultTo(true);
         table.timestamps(false, true);
       })
-      .createTable('user_reviews_likes', table => {
-        table.increments('id');
+      .createTable("user_reviews_likes", table => {
+        table.increments("id");
         table
-          .integer('user_id')
+          .integer("user_id")
           .unsigned()
-          .references('id')
-          .inTable('user')
-          .onDelete('CASCADE');
+          .references("id")
+          .inTable("user")
+          .onDelete("CASCADE");
         table
-          .integer('listing_review_id')
+          .integer("listing_review_id")
           .unsigned()
-          .references('id')
-          .inTable('listing_review')
-          .onDelete('CASCADE');
+          .references("id")
+          .inTable("listing_review")
+          .onDelete("CASCADE");
         table
-          .integer('reply_review_id')
+          .integer("reply_review_id")
           .unsigned()
-          .references('id')
-          .inTable('listing_review')
-          .onDelete('CASCADE');
-        table.string('like_dislike');
-        table.boolean('is_reply').defaultTo(false);
+          .references("id")
+          .inTable("listing_review")
+          .onDelete("CASCADE");
+        table.string("like_dislike");
+        table.boolean("is_reply").defaultTo(false);
         table.timestamps(false, true);
       })
   ]);
@@ -145,13 +146,13 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('listing'),
-    knex.schema.dropTable('listing_image'),
-    knex.schema.dropTable('listing_detail'),
-    knex.schema.dropTable('listing_damage'),
-    knex.schema.dropTable('listing_rental'),
-    knex.schema.dropTable('listing_content'),
-    knex.schema.dropTable('listing_review'),
-    knex.schema.dropTable('user_reviews_likes')
+    knex.schema.dropTable("listing"),
+    knex.schema.dropTable("listing_image"),
+    knex.schema.dropTable("listing_detail"),
+    knex.schema.dropTable("listing_damage"),
+    knex.schema.dropTable("listing_rental"),
+    knex.schema.dropTable("listing_content"),
+    knex.schema.dropTable("listing_review"),
+    knex.schema.dropTable("user_reviews_likes")
   ]);
 };
