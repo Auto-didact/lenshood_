0e811964180812da3b6fbf6d2cc52756c9791594
Bishal Deb <bishaldeb656@gmail.com>
2019-07-27 01:10:28 +0530

    Redirect to My-Listings for users bug removed.

--- modules/listing/client-react/index.jsx
+++ modules/listing/client-react/index.jsx
@@ -1,81 +1,101 @@
-import React from 'react';
+import React from "react";
 
-import ClientModule from '@gqlapp/module-client-react';
-import { translate } from '@gqlapp/i18n-client-react';
-import { IfLoggedIn, AuthRoute } from '@gqlapp/user-client-react/containers/Auth.web';
-import { Route, NavLink } from 'react-router-dom';
-import { MenuItem } from '@gqlapp/look-client-react';
+import ClientModule from "@gqlapp/module-client-react";
+import { translate } from "@gqlapp/i18n-client-react";
+import {
+  IfLoggedIn,
+  AuthRoute
+} from "@gqlapp/user-client-react/containers/Auth.web";
+import { Route, NavLink } from "react-router-dom";
+import { MenuItem } from "@gqlapp/look-client-react";
 
 // To Do
-import { Icon } from 'antd';
+import { Icon } from "antd";
 
-import Listings from './containers/Listings';
+import Listings from "./containers/Listings";
 
-import ListingCatalogueView from './components/ListingCatalogueView';
-import MyListingDetail from './containers/MyListingDetail';
-import ListingEdit from './containers/ListingEdit';
-import ListingAdd from './containers/ListingAdd';
+import ListingCatalogueView from "./components/ListingCatalogueView";
+import MyListingDetail from "./containers/MyListingDetail";
+import ListingEdit from "./containers/ListingEdit";
+import ListingAdd from "./containers/ListingAdd";
 
-import ListingDetail from './containers/ListingDetail';
-import MyListings from './containers/MyListings';
-import ListYourGearOwner from './components/ListYourGearOwner';
-import ListYourGearProduct from './components/ListYourGearProduct';
-import ListYourGearRental from './components/ListYourGearRental';
-import ListingDynamicFieldFormTest from './components/ListingDynamicFieldFormTest';
+import ListingDetail from "./containers/ListingDetail";
+import MyListings from "./containers/MyListings";
+import ListYourGearOwner from "./components/ListYourGearOwner";
+import ListYourGearProduct from "./components/ListYourGearProduct";
+import ListYourGearRental from "./components/ListYourGearRental";
+import ListingDynamicFieldFormTest from "./components/ListingDynamicFieldFormTest";
 
-
-
-import resources from './locales';
-import resolvers from './resolvers';
-
+import resources from "./locales";
+import resolvers from "./resolvers";
 
 const MyListingsNavItemAccount = () => {
   return (
     <div>
       <Icon type="solution" />
-      {'My Listings'}
+      {"My Listings"}
     </div>
   );
 };
 
-const NavLinkAdminWithI18n = translate('listing')(({ t }) => (
+const NavLinkAdminWithI18n = translate("listing")(({ t }) => (
   <NavLink to="/listings" className="nav-link" activeClassName="active">
-    {t('listing:navLinkAdmin')}
+    {t("listing:navLinkAdmin")}
   </NavLink>
 ));
-const NavLinkMyListingsWithI18n = translate('listing')(({ t }) => (
-  <NavLink to="/my-listings" className=" AccDetItem" activeClassName="AccDetItemSelected">
+const NavLinkMyListingsWithI18n = translate("listing")(({ t }) => (
+  <NavLink
+    to="/my-listings"
+    className=" AccDetItem"
+    activeClassName="AccDetItemSelected"
+  >
     <Icon type="solution" />
-    {t('listing:navLinkMyListings')}
+    {t("listing:navLinkMyListings")}
   </NavLink>
 ));
-const NavLinkListYourGearWithI18n = translate('listing')(({ t }) => (
+const NavLinkListYourGearWithI18n = translate("listing")(({ t }) => (
   <NavLink to="/listing/new" className="nav-link" activeClassName="active">
-    {t('listing:navLinkListYourGear')}
+    {t("listing:navLinkListYourGear")}
   </NavLink>
 ));
 
 export default new ClientModule({
   route: [
-
-
-
-
-    <Route exact path="/listings" component={Listings} />,
-    <AuthRoute exact path="/listing/new" redirect="/profile" role={['user', 'admin']} component={ListingAdd} />,
-    <AuthRoute redirect="/profile" role={['user', 'admin']} path="/listing/:id" component={ListingEdit} />,
+    <AuthRoute exact role={["admin"]} path="/listings" component={Listings} />,
+    <AuthRoute
+      exact
+      path="/listing/new"
+      redirect="/profile"
+      role={["user", "admin"]}
+      component={ListingAdd}
+    />,
+    <AuthRoute
+      redirect="/profile"
+      role={["user", "admin"]}
+      path="/listing/:id"
+      component={ListingEdit}
+    />,
     <Route exact path="/listing_catalogue" component={ListingCatalogueView} />,
-
 
     <Route exact path="/my-listings/:id" component={MyListingDetail} />,
 
     // Components
     <Route exact path="/listing-detail/:id" component={ListingDetail} />,
-    <AuthRoute redirect="/profile" role={['user', 'admin']} exact path="/my-listings" component={MyListings} />,
+    <AuthRoute
+      redirect="/profile"
+      role={["user", "admin"]}
+      exact
+      path="/my-listings"
+      component={MyListings}
+    />,
     <Route exact path="/list-your-gear" component={ListYourGearOwner} />,
     <Route exact path="/list-your-gear2" component={ListYourGearProduct} />,
     <Route exact path="/list-your-gear3" component={ListYourGearRental} />,
-    <Route exact path="/listing-dynamic-field" component={ListingDynamicFieldFormTest} />
+    <Route
+      exact
+      path="/listing-dynamic-field"
+      component={ListingDynamicFieldFormTest}
+    />
   ],
   navItemAdmin: [
     <IfLoggedIn>
@@ -110,9 +130,10 @@ export default new ClientModule({
 
   resolver: [resolvers],
 
-  localization: [{ ns: 'listing', resources }]
-
+  localization: [{ ns: "listing", resources }]
 });
 
-export { default as SuggestedCardListComponent } from './components/components/SuggestedCardListComponent';
-export { ImgCamera } from './constants/DefaultImages';
+export {
+  default as SuggestedCardListComponent
+} from "./components/components/SuggestedCardListComponent";
+export { ImgCamera } from "./constants/DefaultImages";
